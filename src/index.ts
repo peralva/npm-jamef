a236ce6f77eb206bfe1b2361c2e8e2781a1c68e2
Bruno Peralva <brunoperalva@hotmail.com>
2025-03-14 12:29:24 -0300

    feat: versão inicial

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,94 @@
-import function1 from './utils/function1';
+import {
+	ApiJamefComBrDocumentosV1NotaFiscalPost,
+	services,
+} from '@peralva/services';
+import tokenIsExpired from './utils/tokenIsExpired';
 
-const var1 = function1();
-console.log(var1);
+export class Jamef {
+	private username: string;
+	private password: string;
+	private isProduction: boolean;
+	private token?: `${string}.${string}.${string}`;
 
-export { function1 };
+	constructor(username: string, password: string, isProduction = true) {
+		this.username = username;
+		this.password = password;
+		this.isProduction = isProduction;
+	}
+
+	private async login(): Promise<`Bearer ${string}.${string}.${string}`> {
+		if (!this.token || tokenIsExpired(this.token)) {
+			const options = {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: {
+					username: this.username,
+					password: this.password,
+				},
+			} as const;
+
+			let responseService;
+
+			if (this.isProduction) {
+				responseService = await services({
+					url: 'https://api.jamef.com.br/auth/v1/login',
+					...options,
+				});
+			} else {
+				responseService = await services({
+					url: 'https://api-qa.jamef.com.br/auth/v1/login',
+					...options,
+				});
+			}
+
+			if (responseService.status !== 200) {
+				throw new Error(
+					'mensagem' in responseService.body
+						? responseService.body.mensagem
+						: responseService.body.error,
+				);
+			}
+
+			this.token = responseService.body.dado[0].accessToken;
+		}
+
+		return `Bearer ${this.token}`;
+	}
+
+	public async sendInvoice(
+		body: ApiJamefComBrDocumentosV1NotaFiscalPost['request']['body'],
+	): Promise<
+		ReturnType<
+			typeof services<{
+				url: 'https://api.jamef.com.br/documentos/v1/nota-fiscal';
+				method: 'POST';
+				body: typeof body;
+				headers: {
+					'Content-Type': 'application/json';
+					Authorization: Awaited<ReturnType<Jamef['login']>>;
+				};
+			}>
+		>
+	> {
+		const options = {
+			method: 'POST',
+			body,
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: await this.login(),
+			},
+		} as const;
+
+		if (this.isProduction) {
+			return await services({
+				url: 'https://api.jamef.com.br/documentos/v1/nota-fiscal',
+				...options,
+			});
+		}
+
+		return await services({
+			url: 'https://api-qa.jamef.com.br/documentos/v1/nota-fiscal',
+			...options,
+		});
+	}
+}
